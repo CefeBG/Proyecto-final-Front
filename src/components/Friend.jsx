a93a80a524edf1406205972abfeb9605ab2d9a64
Cristian <ceffe.94@gmail.com>
2023-01-07 00:44:19 -0300

    buenardo

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `proyecto-final-back-production.up.railway.app/users/${_id}/${friendId}`,
+      `https://proyecto-final-back-production.up.railway.app/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
