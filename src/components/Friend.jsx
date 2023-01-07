32bf0e25641c05af5ab4746ce6ed9d8ff9a9ff6b
Cristian <ceffe.94@gmail.com>
2023-01-07 12:47:38 -0300

    pequeños ajustes

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -42,7 +42,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         <UserImage image={userPicturePath} size="55px" />
         <Box
           onClick={() => {
-            navigate(`/profile/${friendId}`);
+            navigate(`proyecto-final-back-production.up.railway.app/profile/${friendId}`);
             navigate(0);
           }}
         >
