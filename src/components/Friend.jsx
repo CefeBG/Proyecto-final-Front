bf9b1c9108f1d4979d8521ecce216f663db4c4bc
Cristian <ceffe.94@gmail.com>
2023-01-07 13:37:12 -0300

    otro ajuste

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://proyecto-final-back-production.up.railway.app/users/${_id}/${friendId}`,
+      `proyecto-final-back-production.up.railway.app/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
